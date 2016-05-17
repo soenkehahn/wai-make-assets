17ce23feec27bd185d4e4cd3a9cde405fa80bcfd
Sönke Hahn <soenkehahn@gmail.com>
2016-05-17 11:00:26 +0800

    elm: better scaling

--- client/View.elm
+++ client/View.elm
@@ -1,64 +1,83 @@
 module View exposing (..)
 
-import Html.App exposing (..)
-import Html exposing (..)
-import Html.Events exposing (..)
-import Svg exposing (..)
-import VirtualDom exposing (..)
-import Svg.Attributes exposing (..)
-import Keyboard exposing (..)
-import Platform.Sub
-import Time exposing (Time)
+import           Html exposing (..)
+import           Html.App exposing (..)
+import           Html.Events exposing (..)
+import           Keyboard exposing (..)
+import           List exposing (map)
+import           Platform.Sub
+import           String exposing (join)
+import           Svg exposing (..)
+import           Svg.Attributes exposing (..)
+import           Time exposing (Time)
+import           VirtualDom exposing (..)
 
-import State exposing (..)
+import           State exposing (..)
 
 view : State -> Html.Html (Maybe Message)
 view state =
   div [attribute "align" "center"] <|
     Html.text "here" ::
     br [] [] ::
-    Svg.svg [width "600", height "600", viewBox "0 0 600 600"] (viewState state) ::
+    embed 600 600 (viewState state) ::
     []
 
-viewState : State -> List (Svg a)
-viewState state = case state of
+type Box
+  = Box Int Int Int Int
+
+center : Box -> (Float, Float)
+center (Box x1 y1 x2 y2) =
+  (toFloat (x2 - x1) / 2, toFloat (y2 - y1) / 2)
+
+embed : Int -> Int -> (Box -> List (Svg a)) -> Html a
+embed w h contents =
+  let
+    attributes =
+      width (toString w) :: height (toString h) ::
+      viewBox (join " " (List.map toString [0, 0, w, h])) ::
+      []
+    box = Box 0 0 w h
+    background =
+      rect [x "0", y "0", width (toString w), height (toString h), fill "#000"] []
+  in Svg.svg attributes (background :: contents box)
+
+viewState : State -> Box -> List (Svg a)
+viewState state box = case state of
   Empty -> []
   (State i time) ->
     viewI i ++
-    viewTime time
+    viewTime time box
 
 viewI : Int -> List (Svg a)
 viewI i =
-  rect [x "0", y "0", width "10000", height "10000", fill "#000"] [] ::
   rect [x "10", y "10", width "100", height (toString (10 * i)), fill "#88f"] [] ::
   []
 
-viewTime : Time -> List (Svg a)
-viewTime time =
+viewTime : Time -> Box -> List (Svg a)
+viewTime time box =
   let
+    secondHandLen = Basics.min (fst (center box)) (snd (center box))
     secondAngle = handAngles time
-    xEnd = sin secondAngle * 100 + 100
-    yEnd = 0 - cos secondAngle * 100 + 100
+    xEnd = sin secondAngle * secondHandLen + fst (center box)
+    yEnd = 0 - cos secondAngle * secondHandLen + snd (center box)
   in
-    line
-      (x1 "100" :: y1 "100" ::
-       x2 (toString xEnd) :: y2 (toString yEnd) ::
-       stroke "#ff0" ::
-       [])
-      [] ::
-  Debug.log (toString <| handAngles time) []
+    Debug.log "time" <| mkLine (center box) (xEnd, yEnd)
+
+mkLine : (Float, Float) -> (Float, Float) -> List (Svg a)
+mkLine start end =
+  let
+    attributes =
+      x1 (toString (fst start)) :: y1 (toString (snd start)) ::
+      x2 (toString (fst end)) :: y2 (toString (snd end)) ::
+      stroke "#ff0" ::
+      []
+  in [line attributes []]
 
 handAngles : Time -> Float
 handAngles time =
-  let seconds = mod (toFloat <| round (time / 1000)) 60
+  let seconds = Debug.log "seconds" <| toFloat (round (time / 1000) % 60)
       secondAngle = tau * seconds / 60
-  in Debug.log (toString seconds) secondAngle
+  in secondAngle
 
 tau : Float
 tau = 2 * pi
-
-mod : Float -> Float -> Float
-mod a b =
-  if a >= b
-    then mod (a - b) b
-    else a
