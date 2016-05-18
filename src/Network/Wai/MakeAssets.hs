46a10d685f2c287e368c6163d2b3a193b78f30f0
Sönke Hahn <soenkehahn@gmail.com>
2016-05-18 11:32:26 +0800

    add startup checks

--- src/Network/Wai/MakeAssets.hs
+++ src/Network/Wai/MakeAssets.hs
@@ -4,16 +4,20 @@
 module Network.Wai.MakeAssets (serveAssets) where
 
 import           Control.Concurrent
+import           Control.Exception
+import           Control.Monad
 import           Data.Monoid
 import           Data.String.Conversions
-import           Development.Shake hiding (action)
+import           Development.Shake (cmd, Exit(..), Stderr(..), CmdOption(..))
 import           Network.HTTP.Types.Status
 import           Network.Wai
 import           Network.Wai.Application.Static
+import           System.Directory
 import           System.Exit
 
 serveAssets :: IO Application
 serveAssets = do
+  startupChecks
   let fileApp = staticApp $ defaultFileServerSettings "assets/"
   mvar <- newMVar ()
   return $ \ request respond -> do
@@ -26,3 +30,34 @@ serveAssets = do
 
 synchronize :: MVar () -> IO a -> IO a
 synchronize mvar action = modifyMVar mvar $ \ () -> ((), ) <$> action
+
+startupChecks :: IO ()
+startupChecks = do
+  checkExists Dir "client/" $
+    "You should put sources for assets in there."
+  checkExists File "client/Makefile" $ unwords $
+    "Which will be invoked to build the assets." :
+    "It should put compiled assets into 'assets/'." :
+    []
+  checkExists Dir "assets/" $
+    "All files in 'assets/' will be served."
+
+data FileType
+  = File
+  | Dir
+
+checkExists :: FileType -> FilePath -> String -> IO ()
+checkExists typ path hint = do
+  exists <- (isFile doesFileExist doesDirectoryExist) path
+  when (not exists) $ do
+    throwIO $ ErrorCall $ unlines $
+      ("missing " ++ isFile "file" "directory" ++ ": '" ++ path ++ "'") :
+      ("Please create '" ++ path ++ "'.") :
+      ("(" ++ hint ++ ")") :
+      []
+
+  where
+    isFile :: a -> a -> a
+    isFile a b = case typ of
+      File -> a
+      Dir -> b
--- test/Network/Wai/MakeAssetsSpec.hs
+++ test/Network/Wai/MakeAssetsSpec.hs
@@ -2,6 +2,7 @@
 
 module Network.Wai.MakeAssetsSpec where
 
+import           Control.Exception
 import           Control.Lens
 import           Data.ByteString.Lazy (isPrefixOf)
 import           Network.Wai.Handler.Warp
@@ -48,6 +49,42 @@ spec = do
             response <- getWith acceptErrors url
             let body = response ^. responseBody
             body `shouldSatisfy` ("make error:\nerror message\n" `isPrefixOf`)
+
+      context "complains about missing files or directories" $ do
+        it "missing client/" $ do
+          inTempDirectory $ do
+            createDirectoryIfMissing True "assets"
+            let expected = unlines $
+                  "missing directory: 'client/'" :
+                  "Please create 'client/'." :
+                  "(You should put sources for assets in there.)" :
+                  []
+            testWithApplication serveAssets (\ _ -> return ())
+              `shouldThrow` errorCall expected
+
+        it "missing client/Makefile" $ do
+          inTempDirectory $ do
+            createDirectoryIfMissing True "client"
+            createDirectoryIfMissing True "assets"
+            let expected = unlines $
+                  "missing file: 'client/Makefile'" :
+                  "Please create 'client/Makefile'." :
+                  "(Which will be invoked to build the assets. It should put compiled assets into 'assets/'.)" :
+                  []
+            testWithApplication serveAssets (\ _ -> return ())
+              `shouldThrow` errorCall expected
+
+        it "missing assets/" $ do
+          inTempDirectory $ do
+            touch "client/Makefile"
+            let expected = unlines $
+                  "missing directory: 'assets/'" :
+                  "Please create 'assets/'." :
+                  "(All files in 'assets/' will be served.)" :
+                  []
+            catch (testWithApplication serveAssets (\ _ -> return ())) $
+              \ (ErrorCall message) -> do
+                message `shouldBe` expected
 
 acceptErrors :: Options
 acceptErrors = defaults &
